8835aea54ce0d914f0f272579167e446d3d8e6a1
Carlos Tan <carlos.quinto.t@gmail.com>
2021-12-16 16:26:12 +0100

    Add changes to client to use Query Service to get Comments data instead

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
+const CommentList = ({ comments }) => {
+  // Not needed anymore since we get data from Query service
+  // const [comments, setComments] = useState([]);
 
-  const fetchData = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
+  // const fetchData = async () => {
+  //   const res = await axios.get(
+  //     `http://localhost:4001/posts/${postId}/comments`
+  //   );
 
-    setComments(res.data);
-  };
+  //   setComments(res.data);
+  // };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
