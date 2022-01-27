b12b48a7f1b7ee2ef6c7718bb9fcdf7d369bb229
Carlos Tan <carlos.quinto.t@gmail.com>
2022-01-27 22:19:38 +0100

    Add getInitialProps on app level and pass it down to other components

--- ticketing/client/pages/_app.js
+++ ticketing/client/pages/_app.js
@@ -1,9 +1,27 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
 
-export default ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
+      <h1>Header: {currentUser.email}</h1>
       <Component {...pageProps} />
     </div>
   );
 };
+
+// Top level getInitialProps: data for every page
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  // data for specific pages, e.g. LandingPage
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return { pageProps, ...data };
+};
+
+export default AppComponent;
