02f024e4623bc3ae8d4a1dcce7ef6d61c1279c2a
Carlos Tan <carlos.quinto.t@gmail.com>
2022-02-01 21:00:34 +0100

    Finish create ticket request handler

--- ticketing/tickets/src/test/setup.ts
+++ ticketing/tickets/src/test/setup.ts
@@ -2,6 +2,7 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import request from "supertest";
 import { app } from "../app";
+import jwt from "jsonwebtoken";
 
 declare global {
   var signin: () => Promise<string[]>;
@@ -40,15 +41,26 @@ global.signin = async () => {
   //     password,
   //   })
   //   .expect(201);
-  //
-  //
-  // Faking Cookie data for tests
-  // const cookie = response.get("Set-Cookie");
-  // return cookie;
+
+  // Faking Cookie data for tests:
   // Build a JWT payload.  { id, email }
+  const payload = {
+    id: "12313345",
+    email: "test@test.com",
+  };
+
   // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
   // Build session Object. { jwt: MY_JWT }
+  const session = { jwt: token };
+
   // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
   // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
   // return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
 };
